0771a51b0b89016950c050ab307260f6c9ddea13
sanoofp <sanoofp7@gmail.com>
2022-07-10 14:17:42 +0530

    🔧 added rollup

---
+++ rollup.config.ts
@@ -0,0 +1,31 @@
+import pkg from "./package.json";
+import del from "rollup-plugin-delete";
+import dts from "rollup-plugin-dts";
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+  {
+    input: "src/index.ts",
+    plugins: [del({ targets: "lib" }), typescript()],
+    output: [
+      {
+        format: "cjs",
+        file: pkg.main,
+        exports: "default",
+      },
+      {
+        format: "es",
+        file: pkg.module,
+        exports: "default",
+      },
+    ],
+  },
+  {
+    input: "src/index.ts",
+    plugins: [dts()],
+    output: {
+      format: "es",
+      file: pkg.types,
+    },
+  },
+];
